7cd5a8246ed082c885d97522041cc6fec60ebe76
abdulrahman1520 <161980950+abdulrahman1520@users.noreply.github.com>
2024-10-23 02:53:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd 
+from sklearn import datasets
 
 st.title('Automation ML App')
 
-st.write('Hello world!')
+st.write('Explore ML algorithms')
+
+data_set = st.selectbox("select dataset",("heart attack","Breast cancer","iris"))
+Algorithm_name = st.selectbox("select ML algorithm",("SVC","Logistic Regression","Random forest")
+                              
+
