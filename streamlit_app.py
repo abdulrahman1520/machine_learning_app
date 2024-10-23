e3cdf0c91e3337469794152691456b0aa5762242
abdulrahman1520 <161980950+abdulrahman1520@users.noreply.github.com>
2024-10-23 03:00:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,18 @@ st.write('Explore ML algorithms')
 
 data_set = st.selectbox("select dataset",("heart attack","Breast cancer","iris"))
 Algorithm_name = st.selectbox("select ML algorithm",("SVC","Logistic Regression","Random forest"))
-                              
+
+def get_dataset(data_set):
+  if data_set == "heart attack":
+    df = pd.readcsv("E:\heart data\heart.csv")
+  elif data_set = "Breast cancer" : 
+    df = datasets.load_breast_cancer()
+  else :
+    data = datasets.load_iris 
+  x = data.data 
+  y = data.target
+  return x,y 
+
+x,y = get_dataset(data_set)
+st.write("shape of data : ",x.shape)
 
